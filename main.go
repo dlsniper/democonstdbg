36a9cddf9729ea2f4349ca3bd0d7fac85053ac9a
Florin Patan <florinpatan@gmail.com>
2019-11-12 09:47:30 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"play/internal/test"
+)
+
+const AnotherConst = "demo"
+
+func main() {
+	approved := "Approved"
+	ot := AnotherConst
+
+	check(approved, ot)
+}
+
+func check(approved string, ot string) {
+	if approved == test.Approved {
+		fmt.Println("You're in!", ot)
+	}
+}
